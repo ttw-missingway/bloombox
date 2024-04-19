3672c044bd6aba8131091574240a8c847e7380a9
Missingway <ttw.missingway@gmail.com>
2024-04-19 09:28:49 -0500

    buncha stuff

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-let cats = [];
+let cards = [];
 
-async function logCats() {
+async function logCards() {
   const response = await fetch("https://ghibliapi.vercel.app/films");
-  cats = await response.json();
+  cards = await response.json();
 }
 
-$(document).scroll(function() {
-    var scrollPosition = $(window).scrollTop();
+$(document).scroll(function () {
+  var scrollPosition = $(window).scrollTop();
 
-    $('.cat-image').each(function() {
-        var imageOffsetTop = $(this).parent().offset().top; // Get the top position of the image's parent div
-        var speed = 0.05; // Adjust this value to control the speed of the parallax effect
-        var distance = (scrollPosition - imageOffsetTop) * speed + 'px';
+  $(".card-image").each(function () {
+    var imageOffsetTop = $(this).parent().offset().top; // Get the top position of the image's parent div
+    var speed = 0.05; // Adjust this value to control the speed of the parallax effect
+    var distance = (scrollPosition - imageOffsetTop) * speed + "px";
 
-        // Only apply the effect when the image is in the viewport
-        if (scrollPosition + $(window).height() > imageOffsetTop && scrollPosition < imageOffsetTop + $(this).parent().height()) {
-            $(this).css('transform', `translateY(${distance})`);
-        }
-    });
+    // Only apply the effect when the image is in the viewport
+    if (
+      scrollPosition + $(window).height() > imageOffsetTop &&
+      scrollPosition < imageOffsetTop + $(this).parent().height()
+    ) {
+      $(this).css("transform", `translateY(${distance})`);
+    }
+  });
 });
 
 window.onload = async function () {
-  await logCats();
-  $("#main_div").prepend('<div id="cats-container" class="flex-col"></div>');
-  cats.forEach((cat) => {
-    $("#cats-container").prepend(
-      `<div class="cat">
-      <h1>${cat.title}</h1>
-      <div class="img-wrapper"><img class="cat-image" src="${cat.image}" alt="${cat.title}"></div>
-      <p>${cat.description}</p>
-      </div>`);
+  await logCards();
+  $("#main_div").prepend('<div id="card-container" class="flex-col"></div>');
+  cards.forEach((card) => {
+    $("#card-container").prepend(
+      `<div class="card">
+      <h1>${card.title}</h1>
+      <div class="img-wrapper"><img class="card-image" src="${card.image}" alt="${card.title}"></div>
+      <p class="card-copy">${card.description}</p>
+      </div>`
+    );
+  });
+  $(".card-copy").each(function () {
+    if ($(this).prop("scrollHeight") > $(this).innerHeight()) {
+      // Content is overflowing
+      $(this).addClass("is-overflowing");
+    }
   });
 };
